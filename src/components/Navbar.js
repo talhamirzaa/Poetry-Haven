75061f0b21928385a4dcfb60d98a33cb6b4c8201
talhamirzaa <mirzatalha0705@gmail.com>
2024-08-27 23:11:54 +0530

    Navbar logo updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/"><img src="Logo1.png" alt="logo" srcset="" /></Link>
+      <Link className="navbar-brand" to="/"><img src="/logo1.png" alt="logo" srcset="" /></Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
